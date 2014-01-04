4b4c15fae85df2e94befc9af165f92a20c2624fa
Ian Weeks <ianweeks2004@yahoo.com>
2014-01-04 13:06:57 +0000

    Basic strategy is now complete.... Including comments and Java doc.

---
+++ src/QuestionTwo/HumanStrategy.java
@@ -0,0 +1,32 @@
+/*
+ * This program simulates the card game: Cheat.
+ */
+
+package QuestionTwo;
+
+/**
+ *
+ * @author Cypher
+ */
+public class HumanStrategy implements Strategy
+{
+
+    @Override
+    public boolean cheat(Bid b, Hand h)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Bid chooseBid(Bid b, Hand h, boolean cheat)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean callCheat(Hand h, Bid b)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
